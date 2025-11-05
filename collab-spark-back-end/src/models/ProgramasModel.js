86573e4c9a66958aea6d50829a45ba004c28afc4
Gabriel <gabozambrano532@gmail.com>
2025-11-05 14:01:28 -0600

    Se implemento base de datos a la clase de programas

--- collab-spark-back-end/src/models/ProgramasModel.js
+++ collab-spark-back-end/src/models/ProgramasModel.js
@@ -1,28 +1,25 @@
-import { Recursos } from "./RecursosModel.js";
+import mongoose from 'mongoose';
+import { recursoBaseSchema } from './RecursosModel.js';
 
-export class Programas extends Recursos {
-    #tipoPrograma;
-    #fondos;
+// Schema de Programas que extiende el schema base de recursos
+const programasSchema = new mongoose.Schema({
+  ...recursoBaseSchema,
+  tipoPrograma: {
+    type: String,
+    required: [true, 'El tipo de programa es requerido'],
+    trim: true
+  },
+  fondos: {
+    type: String, // Mantener como String para soportar "$30,000 USD por equity del 5%"
+    required: [true, 'Los fondos son requeridos'],
+    trim: true
+  }
+}, {
+  timestamps: true,
+  versionKey: false
+});
 
-    constructor({ nombre, descripcion, tipo, ubicacion, duracion, requisitos, tipoPrograma, fondos }) {
-        super({ nombre, descripcion, tipo, ubicacion, duracion, requisitos });
-        this.#tipoPrograma = tipoPrograma;
-        this.#fondos = fondos;
-    }
+// Crear el modelo
+const Programas = mongoose.model('Programas', programasSchema);
 
-    get tipoPrograma() {
-        return this.#tipoPrograma;
-    }
-
-    get fondos() {
-        return this.#fondos;
-    }
-
-    toJSON() {
-        return {
-            ...super.toJSON(),
-            tipoPrograma: this.#tipoPrograma,
-            fondos: this.#fondos,
-        };
-    }
-}
+export default Programas;
